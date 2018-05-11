77fdfb128e9f7a73f0646f24f2bfdab964e47788
Baize <hejingjun0102@qq.com>
2018-05-11 19:56:05 +0800

    --

--- SortArithmetic/Program.cs
+++ SortArithmetic/Program.cs
@@ -15,9 +15,10 @@ namespace SortArithmetic
             //QSort(list1, 0, iList.Count - 1);
             Test test = new Test();
             test.GetSortStrategy(new QuickSort(iList, 0, iList.Count - 1));
-            test.GetSortStrategy(new BubbleSort(iList));
-            test.GetSortStrategy(new SelectionSort(iList));
+            //test.GetSortStrategy(new BubbleSort(iList));
+            //test.GetSortStrategy(new SelectionSort(iList));
             test.DoSort();
+            Console.WriteLine("Finally:");
             foreach (var item in iList)
             {
                 Console.Write("{0}\t", item);
@@ -35,20 +36,21 @@ namespace SortArithmetic
         }
         static int SortUnit(List<int> list,int low,int high)
         {
-            int key = list[low];
-            while (low<high)
+            int key = list[low];//设定基准数，最终位于区域中间位置，使得左边的数都小于它，右边的数都大于它
+            while (low<high)//当low==high时，第一遍轮询完成
             {
-                while (list[high] >= key && high > low)               
+                while (list[high] >= key && high > low)//从后往前轮询找到一个比左标识小的数               
                     --high;
                 
-                list[low] = list[high];
+                list[low] = list[high];//把较小的数放在左标识的位置
 
                 while (list[low] <= key && high > low)
                     ++low; 
 
-                list[high] = list[low];
+                list[high] = list[low];//把较大的数放在右标识的位置，和上一步的放置结合起来看就是把找到的更大数和更小数作交换。
             }
             list[low] = key;
+
             foreach (var item in list)
             {
                 Console.Write("{0}\t", item);
--- SortArithmetic/QuickSort.cs
+++ SortArithmetic/QuickSort.cs
@@ -24,33 +24,82 @@ namespace SortArithmetic
 
         public void Sort()
         {
-            QSort(Qlist, low, high);          
+            QSort(low, high);          
         }
 
-        private void QSort(List<int> list, int low, int high)
+        private void QSort(int low, int high)
         {  
             if (low >= high)
                 return;
-            int index = SortUnit(Qlist, low, high);
-            QSort(Qlist, low, index - 1);
-            QSort(Qlist, index + 1, high);
+            int index = SortUnit1(low, high);
+            QSort(low, index - 1);
+            QSort(index + 1, high);
         }
-        private int SortUnit(List<int> list, int low, int high)
+        private int SortUnit(int low, int high)
         {
             int key = Qlist[low];
             while (low < high)
             {
+                foreach (var item in Qlist)
+                {
+                    Console.Write("\t{0}", item);
+                }
+                Console.WriteLine();
                 while (Qlist[high] >= key && high > low)
                     --high;
+                int temp = Qlist[low];
                 Qlist[low] = Qlist[high];
+                Console.WriteLine("[{0}]:{1} -> [{2}]:{3}", high, Qlist[high], low, temp);
                 while (Qlist[low] <= key && high > low)
                     ++low;
+                int temp1 = Qlist[high];
                 Qlist[high] = Qlist[low];
+                Console.WriteLine("[{0}]:{1} -> [{2}]:{3}", low, Qlist[low], high, temp1);
+
+                foreach (var item in Qlist)
+                {
+                    Console.Write("\t{0}", item);                    
+                }
+                Console.WriteLine();
             }
             Qlist[low] = key;
 
             return high;
         }
+        private int SortUnit1(int low, int high)
+        {
+            int i, j, temp;
+            temp = Qlist[low];
+            i = low;
+            j = high;
+            while (i!=j)
+            {
+                while (Qlist[j]>=temp&&i<j)
+                {
+                    j--;
+                }
+                while (Qlist[i]<=temp&&i<j)
+                {
+                    i++;
+                }
+                if (i<j)
+                {
+                    Exchange(i, j);
+                }
+                
+            }
+            Qlist[low] = Qlist[i];
+            Qlist[i] = temp;
+
+            return i;
+        }
+        private void Exchange(int a,int b)
+        {
+            int temp = Qlist[a];
+            Qlist[a] = Qlist[b];
+            Qlist[b] = temp;
+        }
+
     }
 
 }
